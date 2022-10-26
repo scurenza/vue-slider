ef957062d9e648e67ba01ea42e6afb2e17ede51b
scurenza <fedechiar@hotmail.it>
2022-10-26 17:05:15 +0200

    problemi con variabile non definita

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-const slides = [
-    {
-        image: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        image: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum.',
-    },
-    {
-        image: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        image: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-    },
-    {
-        image: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-    }
-];
+// const slides = [
+//     {
+//         image: 'img/01.jpg',
+//         title: 'Svezia',
+//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+//     },
+//     {
+//         image: 'img/02.jpg',
+//         title: 'Svizzera',
+//         text: 'Lorem ipsum.',
+//     },
+//     {
+//         image: 'img/03.jpg',
+//         title: 'Gran Bretagna',
+//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     },
+//     {
+//         image: 'img/04.jpg',
+//         title: 'Germania',
+//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+//     },
+//     {
+//         image: 'img/05.jpg',
+//         title: 'Paradise',
+//         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+//     }
+// ];
 
-console.log(slides);
+// console.log(slides);
 
 // Descrizione:
 // Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
@@ -46,7 +46,51 @@ const { createApp } = Vue
 createApp({
     data () {
         return {
-
+            slides: [
+                {
+                    image: 'img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    image: 'img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum.',
+                },
+                {
+                    image: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    image: 'img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    image: 'img/05.jpg',
+                    title: 'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                }
+            ],
+            currentSlide: 0
+        };
+    },
+    methods: {
+        nextImg: function() {
+            if(currentSlide < this.slides.length - 1) {
+                this.currentSlide++;
+            } else {
+                this.currentSlide = 0;
+            }
+        },
+        prevImg: function() {
+            if(currentSlide > 0) {
+                this.currentSlide--;
+            } else {
+                this.currentSlide = this.slides.length - 1;
+            }
         }
     }
 }).mount('#app')
+
